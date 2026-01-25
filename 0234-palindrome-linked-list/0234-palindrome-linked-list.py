37a20aa424d919b3f02f9ad6d988326c14622115
Sharad Shukla <sharadshukla@users.noreply.github.com>
2026-01-25 22:07:00 +0100

    Time: 39 ms (40.81%), Space: 42.6 MB (28.68%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,16 +5,50 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        #TC: O(n)   SC: O(n)
+        # if not head or not head.next:
+        #     return True
+
+        # listLL = []
+        # curr = head
+        # while curr:
+        #     listLL.append(curr.val)
+        #     curr = curr.next
+        
+        # return listLL == listLL[::-1]
+
         if not head or not head.next:
             return True
-
-        listLL = []
-        curr = head
-        while curr:
-            listLL.append(curr.val)
-            curr = curr.next
         
-        return listLL == listLL[::-1]
+        slow = fast = head
+
+        #finding the mid point
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # reversing the right part after mid point:
+        curr = slow
+        prev = None
+
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+
+        # matching the right and left part
+        left, right = head, prev
+        
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        
+        return True
+        
+
 
 
             
