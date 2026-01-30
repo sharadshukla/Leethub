d678d07346fd5ff3c2633047fdad07cc78b27744
Sharad Shukla <sharadshukla@users.noreply.github.com>
2026-01-30 08:19:03 +0100

    Time: 0 ms (100%), Space: 19.2 MB (39.6%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -7,18 +7,18 @@ class Solution:
                 continue
             if not stack:
                 return False
+
+            if i == ')' and stack[-1] == '(':
+                stack.pop()
+                continue
+            elif i == ']' and stack[-1] == '[':
+                stack.pop()
+                continue
+            elif i == '}' and stack[-1] == '{':
+                stack.pop()
+                continue
             else:
-                if i == ')' and stack[-1] == '(':
-                    stack.pop()
-                    continue
-                elif i == ']' and stack[-1] == '[':
-                    stack.pop()
-                    continue
-                elif i == '}' and stack[-1] == '{':
-                    stack.pop()
-                    continue
-                else:
-                    return False
+                return False
         
         return not stack
             
