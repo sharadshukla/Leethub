2a143a302107698b23c4c8cfbd9dbb5684d7314e
Sharad Shukla <sharadshukla@users.noreply.github.com>
2026-02-28 15:06:52 +0100

    Time: 0 ms (100%), Space: 19.4 MB (59.9%) - LeetHub

---
+++ 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findSecondMinimumValue(self, root: Optional[TreeNode]) -> int:
+        self.min1 = root.val
+        self.min2 = float('inf')
+
+        def dfs(node):
+            if not node:
+                return -1
+            if self.min1 < node.val < self.min2:
+                self.min2 = node.val
+            elif self.min1 == node.val:
+                dfs(node.left)
+                dfs(node.right)
+            
+        dfs(root)
+        return self.min2 if self.min2 < float('inf') else -1
+
+        
