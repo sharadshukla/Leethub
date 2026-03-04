1f8738cefbe3a2a8f0fc8caa88f66ccf11eafa18
Sharad Shukla <sharadshukla@users.noreply.github.com>
2026-03-04 08:44:28 +0100

    Time: 134 ms (39.22%), Space: 31.7 MB (95.67%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -7,15 +7,77 @@
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
 
+        self.count = 0
+        # pre prder traversal
         def dfs(node, maxVal):
+
             if not node:
                 return 0
             
-            res = 1 if node.val >= maxVal else 0
-            maxVal = max(maxVal, node.val)
-            
-            res += dfs(node.left, maxVal)
-            res += dfs(node.right, maxVal)
-            return res
+            if node.val >= maxVal:
+                self.count +=1
+                maxVal = max(maxVal, node.val)
 
-        return dfs(root, float('-inf'))
+            dfs(node.left, maxVal)
+            dfs(node.right, maxVal)
+        
+        dfs(root, float('-inf'))
+        return self.count
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # def dfs(node, maxVal):
+        #     # Checking if node exists
+        #     if not node:
+        #         return 0
+            
+        #     # If value of node is greater than max value, then it is a good node
+        #     # the good node count is assigned
+        #     res = 1 if node.val >= maxVal else 0
+
+        #     # Max value is chosen
+        #     maxVal = max(maxVal, node.val)
+            
+        #     ## recursively the counter is incremented for all good nodes for
+        #     # all the left subtrees
+        #     res += dfs(node.left, maxVal)
+
+        #     ## recursively the counter is incremented for all good nodes for
+        #     # all the right subtrees
+        #     res += dfs(node.right, maxVal)
+
+        #     # returning the count value
+        #     return res
+
+        # # returning the result of dfs call
+        # return dfs(root, float('-inf'))
